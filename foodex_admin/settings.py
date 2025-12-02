5eeae59f19e88b029a55f11e16591a001cd6dde8
Rusty <rustam_ibrayev@list.ru>
2025-12-02 20:34:59 +0500

    Start local dev

--- foodex_admin/settings.py
+++ foodex_admin/settings.py
@@ -12,9 +12,17 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# !!! Comment this block when run in the docker container !!!
+ENV_FILE = BASE_DIR.parent / 'envs' / 'admin_service_dev.env'
+if ENV_FILE.exists():
+    load_dotenv(ENV_FILE)
+# !!! Comment this block when run in the docker container !!!
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
